400b9ae0440a06df585fef1932b1a345bc65a1e3
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2023-04-20 23:29:24 +0300

    start of graphql

---
+++ src/api/resolvers/index.ts
@@ -0,0 +1,6 @@
+import userResolver from './userResolver';
+import postResolver from './postResolver';
+import likeResolver from './likeResolver';
+import groupResolver from './groupResolver';
+
+export default [userResolver, postResolver, likeResolver, groupResolver];
